fa4d621a10c5838857b003f0e9268353d22033ee
Nikhil Khatu <nikhil.khatu.ece@gmail.com>
2024-12-08 23:26:52 -0500

    groupAnagrams

--- src/main.cpp
+++ src/main.cpp
@@ -9,97 +9,95 @@ using namespace std;
 class Solution {
 
 public:
-  /*
-  // Solution using Sorting
-      vector<int> twoSum(const vector<int>& nums, const int target) {
+  // Sorting Solution
+  // Time complexity: O(m*nlogn)
+  // Space complexity: O(m*n)
+  // m is the number of strings and n is the length of the longest string
+  vector<vector<string>> groupAnagrams(vector<string> &strs) {
 
-          // Create a new array A of type pair<int, int>
-          vector<pair<int, int>> A;
+    // Create a new hash map named res
+    unordered_map<string, vector<string>> res;
 
-          // traverse nums
-          for (int i = 0; i < nums.size(); i++) {
-
-              // Push pair{value, index} into new array
-              A.push_back({nums[i], i});
-          }
-
-          // sort the new Pair array (sort by nums[i])
-          sort(A.begin(), A.end());
-
-
-          // print out sorted array (sanity check)
-          for(auto x: A){
-              cout << x.first << ' ' << x.second << endl;
-          }
-
-          //initialize i and j (two pointers)
-          int i = 0, j = nums.size() - 1;
-
-          // iterate through  sorted A
-          while (i < j) {
-
-              // add both values
-              int cur = A[i].first + A[j].first;
-
-              // if added value equals target return the indices
-              if (cur == target) {
-                  return {min(A[i].second, A[j].second),
-                          max(A[i].second, A[j].second)};
-              }
-
-              // if added value is less than the target increment i pointer
-              else if (cur < target) {
-                  i++;
-              }
-
-              // if added value is greater than the target decrement j pointer
-              else {
-                  j--;
-              }
-          }
-          return {};
-      }
-      */
-
-  vector<int> twoSum(vector<int> &nums, int target) {
-
-    // size of nums
-    int n = nums.size();
-
-    // create unordered hash map
-    unordered_map<int, int> prevMap;
-
-    // iterate through nums
-    for (int i = 0; i < n; i++) {
-      // find difference between value and target
-      int diff = target - nums[i];
-
-      // Look for diff. If it exists in the hash map return indices
-      if (prevMap.find(diff) != prevMap.end()) {
-        return {prevMap[diff], i};
-      }
-      // if diff does not already exist, insert key/ value pair
-      prevMap.insert({nums[i], i});
-
-      // Sanity check... Print out values
-      for (auto x : prevMap) {
-        cout << x.first << ' ' << x.second << endl;
-      }
-      cout << "---" << "\n";
+    // for each given string- use the sorted string as the key and push the
+    // string.
+    for (const auto &s : strs) {
+      // copy string to a new variable
+      string sortedS = s;
+      // sort the string by character
+      sort(sortedS.begin(), sortedS.end());
+      // Push the sorted string to the hash map
+      res[sortedS].push_back(s);
     }
-    return {};
+
+    // Build the resulting vector of string vectors
+    vector<vector<string>> result;
+    for (auto &pair : res) {
+      result.push_back(pair.second);
+    }
+
+    // return the vector of string vectors
+    return result;
   }
+
+  // Hash Table solution
+  // Time complexity: O(m*n)
+  // Space complexity: O(m)
+  // m is the number of strings and n is the length of the longest string
+
+  /*
+      vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+          // create a new hash map named res
+          // key- string, value- vector<string>
+          unordered_map<string, vector<string>> res;
+
+          // iterate through strings to put them into the res hash map
+          for (const auto& s : strs) {
+
+              // Create a vector to keep count of each char in the string
+              vector<int> count(26, 0);
+              for (char c : s) {
+                  count[c - 'a']++;
+              }
+
+              // Create a unique string key for each anagram
+              string key = to_string(count[0]);
+              for (int i = 1; i < 26; ++i) {
+                  key += ',' + to_string(count[i]);
+              }
+
+              // Push the string value to the hash map using the created key
+              res[key].push_back(s);
+          }
+
+
+          // Create a new vector of string vectors named result(for return)
+          vector<vector<string>> result;
+
+          // for each key/ value pair. pop the anagram group values into the
+     vector of vector strings for (const auto& pair : res) {
+              result.push_back(pair.second);
+          }
+
+          // return value
+          return result;
+      }
+  */
 };
 
 int main() {
 
   Solution sol;
 
-  vector<int> n{3, 5, 6, 2, 9};
-  vector<int> targ = sol.twoSum(n, 7);
+  vector<string> test_str{"act", "pots", "tops", "cat", "stop", "hat"};
 
-  for (auto x : targ) {
-    cout << x << endl;
+  vector<vector<string>> result = sol.groupAnagrams(test_str);
+
+  for (auto x : result) {
+    for (auto s : x) {
+      cout << s << "," << endl;
+    }
+    cout << "-->" << endl;
   }
   return 0;
 }
